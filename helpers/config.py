247e158868f22fe9ec455c6e29bbc6dd566b2c17
fannarfreyr <fnnr.freyr@gmail.com>
2025-12-18 21:05:44 +0100

    Fixed config.py and env

--- helpers/config.py
+++ helpers/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ENDPOINT = "https://au803280-foundry.services.ai.azure.com/openai/v1/"
+ENDPOINT = os.getenv("AZURE_ENDPOINT")
 MODEL_NAME = "DeepSeek-V3-0324"
 DEPLOYMENT_NAME = "DeepSeek-V3-0324"
 API_KEY = os.getenv("API_KEY")
